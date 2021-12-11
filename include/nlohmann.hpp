503eea5bd34d83a54404de18ee35098dfe769e65
Stas Vavich <someonesp@inbox.ru>
2021-12-11 04:35:18 -0800

    0.2

---
+++ include/nlohmann.hpp
@@ -0,0 +1,8 @@
+//
+// Created by noname on 12/11/21.
+//
+
+#ifndef TEMPLATE_NLOHMANN_HPP
+#define TEMPLATE_NLOHMANN_HPP
+
+#endif  // TEMPLATE_NLOHMANN_HPP
--- include/students.hpp
+++ include/students.hpp
@@ -3,6 +3,40 @@
 #ifndef INCLUDE_STUDENTS_HPP_
 #define INCLUDE_STUDENTS_HPP_
 
-auto example() -> void;
+#include <vector>
+#include <string>
+#include <any>
+
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+
+using nlohmann::json;
+
+json takeJson(int argc, char** argv);
+
+using nlohmann::json;
+
+struct Student {
+ public:
+  Student();
+  Student(std::string _name, std::any _group, std::any _avg, std::any _debt);
+  bool operator==(Student const & student) const;
+  std::string name;
+  std::any group;
+  std::any avg;
+  std::any debt;
+};
+
+void print(Student& student, std::ostream& os);
+
+void print(std::string s1, std::string s2, std::string s3, std::string s4, std::ostream& os);
+
+void print(std::vector<Student>& students, std::ostream& os);
+
+void from_json(const json& j, Student& s);
+
+std::vector<Student> parsingJson(json& data);
 
 #endif // INCLUDE_STUDENTS_HPP_
